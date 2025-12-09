5c2115f6818123dbed05c9ea6671f28d0096637b
Matthew Rozendaal <rozendaal.matthew@gmail.com>
2025-12-09 00:27:08 -0600

    Do report work for EquipmentProfitReport

---
+++ Milestone 3 Work/EquipmentProfitReport.py
@@ -0,0 +1,24 @@
+import DisplayTableData as TableData
+class Table:
+    pass
+def main():
+    # Get a database connection
+    conn = TableData.GetDatabaseConnection()
+    if conn is not None:
+        print("Successfully connected to the database.")
+    else:
+        print("Failed to connect to the database.")
+        return
+    
+    # Create a cursor
+    cursor = conn.cursor()
+
+    # Get Report from view
+    TableData.display_table(cursor, "EquipmentProfitViewWithRentals", True)
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
